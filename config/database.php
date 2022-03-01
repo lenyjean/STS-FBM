48da9df83ba472ac9debcefb3ff4f0ff392e9e9f
lenyjean <99113542+lenyjean@users.noreply.github.com>
2022-03-01 20:33:03 +0800

    ulit daw

--- config/database.php
+++ config/database.php
@@ -68,7 +68,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'stsfbm_new'),
+            'database' => env('DB_DATABASE', 'sts-fbm_database'),
             'username' => env('DB_USERNAME', 'postgres'),
             'password' => env('DB_PASSWORD', 'chuckieyOOh'),
             'charset' => 'utf8',
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//route group
+// Route::middleware(['auth'])->group(function () {
+// });
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+require __DIR__ . '/auth.php';
